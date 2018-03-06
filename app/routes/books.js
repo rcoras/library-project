89843c533d250db9374f96c27d3009f6db39ea6b
rcoras <rcoras@yahoo.com>
2018-03-06 10:11:46 -0500

    Adds delete book button and functionality
    Adds delete button to each book in hbs and adds deleteBook action to js file

--- app/routes/books.js
+++ app/routes/books.js
@@ -4,4 +4,10 @@ export default Route.extend({
   model () {
     return this.get('store').findAll('book');
   },
+  actions: {
+    deleteBook (book) {
+      console.log('this is book', book)
+      return book.destroyRecord()
+    },
+  }
 });
