d3b736a1def95ebc42d78be9e519b719dc332fea
rcoras <rcoras@yahoo.com>
2018-03-08 16:13:58 -0500

    Rolls back changes to earlier version w/o comments
    Rolls back to books new being a nested route. Removes comments feature

---
+++ app/routes/books-new.js
@@ -0,0 +1,27 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model () {
+    return this.get('store').createRecord('book', {}).rollbackAttributes()
+  },
+  actions: {
+  save (newBook) {
+    console.log('save run')
+    return newBook.save()
+      .then(() => this.transitionTo('books'))
+    }
+  }
+});
+
+// export default Route.extend({
+//   model () {
+//     return this.get('store').createRecord('book', {})
+//   },
+//   actions: {
+//   save (newBook) {
+//     console.log('save run')
+//     return newBook.save()
+//       .then(() => this.transitionTo('books'))
+//     }
+//   }
+// });
